249a9742743ef4efd27a87705e9b2afc7f12aa42
SatarupaTech <satarupakundu16@gmail.com>
2025-04-03 18:21:00 +0530

    Create LinkShortener.py

---
+++ LinkShortener.py
@@ -0,0 +1,12 @@
+import pyshorteners
+
+#get the link from the user
+link = input("Enter the link:")
+
+#create a shortener object
+shortener = pyshorteners.Shortener()
+
+#shorten the link using TinyURL
+shortened_link = shortener.tinyurl.short(link)
+#print the shortened link
+print("Shortened Link:", shortened_link)
