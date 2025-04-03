036841443e97486b848f0ce4a88d63de2ba51a27
SatarupaTech <satarupakundu16@gmail.com>
2025-04-03 17:24:26 +0530

    Create GuessTheNumber.py

---
+++ GuessTheNumber.py
@@ -0,0 +1,15 @@
+import random
+
+number = random.randint(1,10)
+
+for i in range(3):
+    user = int(input("Guess the number(1-10): "))
+    
+    if user == number:
+        print(f"Hurray! You gussed the number right, it's{number}.")
+        break
+    else:
+        print("Wrong guess.Try again!")
+        
+if user != number:
+    print(f"Sorry, you've used all attempts. The number was {number}.")
